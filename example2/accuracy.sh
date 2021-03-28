5455775103fd170d0a71725ee85d02f642b20193
Alexander Po-Yen Wu <alexwu@dragon.csail.mit.edu>
2021-03-27 22:53:04 -0400

    updated script for accuracy eval

--- example2/accuracy.sh
+++ example2/accuracy.sh
@@ -8,15 +8,15 @@ base_dir="~/sharenet/example2"
 results_dir="${base_dir}/data"
 
 file_name="pidc.edges.txt.gz"
-python -u /data/cb/alexwu/sharenet/sharenet_accuracy.py -d $base_dir -r $results_dir -K 24 -f $file_name -rn "STRING" 
-python -u /data/cb/alexwu/sharenet/sharenet_accuracy.py -d $base_dir -r $results_dir -K 24 -f $file_name -rn "nonspecific_chip" 
-python -u /data/cb/alexwu/sharenet/sharenet_accuracy.py -d $base_dir -r $results_dir -K 24 -f $file_name -rn "specific_chip" 
+python -u ~/sharenet/sharenet_accuracy.py -d $base_dir -r $results_dir -K 24 -f $file_name -rn "STRING" 
+python -u ~/sharenet/sharenet_accuracy.py -d $base_dir -r $results_dir -K 24 -f $file_name -rn "nonspecific_chip" 
+python -u ~/sharenet/sharenet_accuracy.py -d $base_dir -r $results_dir -K 24 -f $file_name -rn "specific_chip" 
 
 base_dir="/data/cb/alexwu/sharenet/example2"
 results_dir="${base_dir}/results"
 
 file_name="sharenet.nc10.thresh1.0.pidc.edges.txt"
-python -u /data/cb/alexwu/sharenet/sharenet_accuracy.py -d $base_dir -r $results_dir -K 24 -f $file_name -rn "STRING" 
-python -u /data/cb/alexwu/sharenet/sharenet_accuracy.py -d $base_dir -r $results_dir -K 24 -f $file_name -rn "nonspecific_chip" 
-python -u /data/cb/alexwu/sharenet/sharenet_accuracy.py -d $base_dir -r $results_dir -K 24 -f $file_name -rn "specific_chip" 
+python -u ~/sharenet/sharenet_accuracy.py -d $base_dir -r $results_dir -K 24 -f $file_name -rn "STRING" 
+python -u ~/sharenet/sharenet_accuracy.py -d $base_dir -r $results_dir -K 24 -f $file_name -rn "nonspecific_chip" 
+python -u ~/sharenet/sharenet_accuracy.py -d $base_dir -r $results_dir -K 24 -f $file_name -rn "specific_chip" 
 
